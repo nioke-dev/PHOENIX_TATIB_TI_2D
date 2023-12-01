2e92161a63d7d37fbc986d2afec29f6a02ad232c
Yonatan Efrassetyo <yoechannelpjr@gmail.com>
2023-12-01 10:39:25 +0700

    tambah logo

--- app/views/templates/sidebar.php
+++ app/views/templates/sidebar.php
@@ -34,23 +34,19 @@
                       <li class="sidebar-item">
                           <a class="sidebar-link" href="<?= BASEURL; ?>/mahasiswa" aria-expanded="false">
                               <span>
-                                  <i class="ti ti-article"></i>
+                                  <!-- <i class="bi bi-people"></i> -->
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-people" viewBox="0 0 17 17">
+  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+</svg>
                               </span>
                               <span class="hide-menu">Mahasiswa</span>
                           </a>
                       </li>
                       <li class="sidebar-item">
-                          <a class="sidebar-link" href="<?= BASEURL; ?>/about" aria-expanded="false">
-                              <span>
-                                  <i class="ti ti-alert-circle"></i>
-                              </span>
-                              <span class="hide-menu">About</span>
-                          </a>
-                      </li>
-                      <li class="sidebar-item">
                           <a class="sidebar-link" href="<?= BASEURL; ?>/dosen" aria-expanded="false">
                               <span>
-                                  <i class="ti ti-cards"></i>
+                                  <!-- <i class="ti ti-cards"></i> -->
+                                  <i class="ti ti-id"></i>
                               </span>
                               <span class="hide-menu">Dosen</span>
                           </a>
@@ -82,19 +78,20 @@
                       <li class="sidebar-item">
                           <a class="sidebar-link" href="<?= BASEURL; ?>/tatib" aria-expanded="false">
                               <span>
-                                  <i class="ti ti-typography"></i>
+                                  <!-- <i class="ti ti-typography"></i> -->
+                                  <i class="ti ti-article"></i>
                               </span>
                               <span class="hide-menu">Tata Tertib</span>
                           </a>
                       </li>
                       <li class="nav-small-cap">
                           <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                          <span class="hide-menu">AUTH</span>
+                          <span class="hide-menu">SETTINGS</span>
                       </li>
                       <li class="sidebar-item">
                           <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
                               <span>
-                                  <i class="ti ti-login"></i>
+                                  <i class="ti ti-mood-happy"></i>
                               </span>
                               <span class="hide-menu">Login</span>
                           </a>
@@ -105,6 +102,14 @@
                                   <i class="ti ti-user-plus"></i>
                               </span>
                               <span class="hide-menu">Register</span>
+                          </a>
+                      </li>
+                      <li class="sidebar-item">
+                          <a class="sidebar-link" href="<?= BASEURL; ?>/about" aria-expanded="false">
+                              <span>
+                                  <i class="ti ti-alert-circle"></i>
+                              </span>
+                              <span class="hide-menu">About</span>
                           </a>
                       </li>
                       <li class="nav-small-cap">

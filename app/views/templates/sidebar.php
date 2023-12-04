0850333dd7e6b3794b2ab7e5cdcd383f080f1583
WHARN0 <variosrio@gmail.com>
2023-12-05 04:18:45 +0700

    REMOVING UNUSED ICON

--- app/views/templates/sidebar.php
+++ app/views/templates/sidebar.php
@@ -102,28 +102,6 @@
                           <span class="hide-menu">SETTINGS</span>
                       </li>
                       <li class="sidebar-item">
-                          <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                              <span>
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 15 15">
-                                      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                                      <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                                  </svg>
-                              </span>
-                              <span class="hide-menu">Login</span>
-                          </a>
-                      </li>
-                      <li class="sidebar-item">
-                          <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
-                              <span>
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 15 14">
-                                      <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z" />
-                                      <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5" />
-                                  </svg>
-                              </span>
-                              <span class="hide-menu">Register</span>
-                          </a>
-                      </li>
-                      <li class="sidebar-item">
                           <a class="sidebar-link" href="<?= BASEURL; ?>/AdminControllers/about" aria-expanded="false">
                               <span>
                                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 15 16">
@@ -132,26 +110,6 @@
                                   </svg>
                               </span>
                               <span class="hide-menu">About</span>
-                          </a>
-                      </li>
-                      <li class="nav-small-cap">
-                          <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                          <span class="hide-menu">EXTRA</span>
-                      </li>
-                      <li class="sidebar-item">
-                          <a class="sidebar-link" href="./icon-tabler.html" aria-expanded="false">
-                              <span>
-                                  <i class="ti ti-mood-happy"></i>
-                              </span>
-                              <span class="hide-menu">Icons</span>
-                          </a>
-                      </li>
-                      <li class="sidebar-item">
-                          <a class="sidebar-link" href="./sample-page.html" aria-expanded="false">
-                              <span>
-                                  <i class="ti ti-aperture"></i>
-                              </span>
-                              <span class="hide-menu">Sample Page</span>
                           </a>
                       </li>
                   </ul>

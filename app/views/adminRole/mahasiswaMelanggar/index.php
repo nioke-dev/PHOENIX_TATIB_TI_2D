edf7f8922cb60268c9cab62cb6f5f08cfa6a11d5
Nurul Mustofa <nioke8090@gmail.com>
2023-12-07 14:30:38 +0700

    mahasiswa Melanggar fix bug

---
+++ app/views/adminRole/mahasiswaMelanggar/index.php
@@ -0,0 +1,124 @@
+<!-- Flash message -->
+<div class="row">
+    <div class="col-lg-6">
+        <?php Flasher::flash(); ?>
+    </div>
+</div>
+
+<!-- Daftar Mahasiswa -->
+<div class="row">
+    <h3>Daftar Mahasiswa Melanggar</h3>
+    <table id="example" class="table table-striped" style="width:100%">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nim Mahasiswa</th>
+                <th>Nama</th>
+                <th>Kelas</th>
+                <th>Status Sanksi</th>
+                <th>Tingkat Sanksi</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no = 1;
+            if (empty($data['mhs_melanggar'])) : ?>
+                <tr>
+                    <td>
+                        <div class="alert alert-danger" role="alert">
+                            Tidak ada data terkait.
+                        </div>
+                    </td>
+                </tr>
+                <?php else :
+                foreach ($data['mhs_melanggar'] as $mhs_melanggar) : ?>
+                    <tr>
+                        <th><?= $no++; ?></th>
+                        <td><?= $mhs_melanggar['nim_mahasiswa']; ?></td>
+                        <td><?= $mhs_melanggar['nama_mahasiswa']; ?></td>
+                        <td><?= $mhs_melanggar['kelas_mahasiswa']; ?></td>
+                        <td><?= $mhs_melanggar['status_sanksi']; ?></td>
+                        <td><?= $mhs_melanggar['tingkat_sanksi']; ?></td>
+                        <td>
+                            <a href="<?= BASEURL; ?>/AdminControllers/mahasiswaMelanggar/detail/<?= $mhs_melanggar['nim_mahasiswa']; ?>" class="badge bg-primary float-right tampilModalDetailMahasiswaMelanggar" data-bs-toggle="modal" data-bs-target="#detailModalMahasiswaMelanggar" data-nim_mahasiswa="<?= $mhs_melanggar['nim_mahasiswa']; ?>">Detail</a>
+                            <a href="<?= BASEURL; ?>/AdminControllers/mahasiswaMelanggar/ubah/<?= $mhs_melanggar['nim_mahasiswa']; ?>" class="badge bg-success float-right tampilModalUbahMahasiswa" data-bs-toggle="modal" data-bs-target="#formModalMahasiswaMelanggar" data-nim_mahasiswa="<?= $mhs_melanggar['nim_mahasiswa']; ?>">ubah</a>
+                        </td>
+                    </tr>
+            <?php endforeach;
+            endif; ?>
+        </tbody>
+    </table>
+</div>
+
+</div>
+
+<!-- Modal Detail Mahasiswa -->
+<div class="modal fade" id="detailModalMahasiswaMelanggar" tabindex="-1" aria-labelledby="detailModalMahasiswaMelanggarLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="detailModalMahasiswaMelanggarLabel">Detail Data Mahasiswa</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <p><strong>NIM Mahasiswa:</strong> <span id="detailNimMahasiswaMelanggar"></span></p>
+                </div>
+                <div class="form-group">
+                    <p><strong>Nama Mahasiswa:</strong> <span id="detailNamaMahasiswaMelanggar"></span></p>
+                </div>
+                <div class="form-group">
+                    <p><strong>Kelas Mahasiswa:</strong> <span id="detailKelasMahasiswaMelanggar"></span></p>
+                </div>
+                <div class="form-group">
+                    <p><strong>Program Studi Mahasiswa:</strong> <span id="detailProdiMahasiswaMelanggar"></span></p>
+                </div>
+                <div class="form-group">
+                    <p><strong>Email Mahasiswa:</strong> <span id="detailEmailMahasiswaMelanggar"></span></p>
+                </div>
+                <div class="form-group">
+                    <p><strong>Status Sanksi Mahasiswa:</strong> <span id="detailStatusSanksiMahasiswaMelanggar"></span></p>
+                </div>
+                <div class="form-group">
+                    <p><strong>Tingkat Sanksi Mahasiswa:</strong> <span id="detailTingkatSanksiMahasiswaMelanggar"></span></p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal Tambah/Edit Mahasiswa -->
+<div class="modal fade" id="formModalMahasiswaMelanggar" tabindex="-1" aria-labelledby="formModalMahasiswaMelanggarLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="formModalMahasiswaMelanggarLabel">Ubah Data Mahasiswa Melanggar</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL; ?>/AdminControllers/mahasiswaMelanggar/ubah" method="post">
+                    <input type="hidden" name="nim_mahasiswa" id="nim_mahasiswa">
+                    <div class="form-group">
+                        <label for="id_tingkatSanksi">Tingkat Sanksi</label>
+                        <select class="form-control choices-single" id="id_tingkatSanksi" name="id_tingkatSanksi" autocomplete="off" required>
+                            <option></option>
+                            <option value="6">V</option>
+                            <option value="5">IV</option>
+                            <option value="4">III</option>
+                            <option value="3">II</option>
+                            <option value="2">I/II</option>
+                            <option value="1">I</option>
+                        </select>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
+                </form>
+            </div>
+        </div>
+    </div>
